714d133d4985a387029723ac656b71459c683210
PMyshkin <PMyshkin@outlook.com>
2022-02-20 14:32:57 +0330

    Prime

---
+++ prime_check.py
@@ -0,0 +1,21 @@
+#*Author : Prince.Myshkin
+
+#*"You should pass us by and forgive us our happiness," said the prince in a low voice!!!"
+#*“Beauty will save the world"!!
+
+########################################################################################################
+
+Number = int(input())
+a=2
+Prime=2
+while a < Number -1:
+    if Number % a == 0:
+        print(False)
+        break
+    a += 1
+    Prime +=1
+    if Prime == Number -1:
+        print(True)
+
+
+
